4b26e9ca20de8bca6dcfb6a36f75455d498586b8
jay karale <jaykarale06@gmail.com>
2026-01-04 23:56:53 +0530

    second correction/corrected

--- src/MaxMinTracker.hpp
+++ src/MaxMinTracker.hpp
@@ -1,8 +1,17 @@
 #ifndef MAXMINTRACKER_HPP
 #define MAXMINTRACKER_HPP
+#include<queue>
+#include<vector>
 
 class MaxMinTracker{
-    
+    public:
+       void storePrice(double price);
+       void showMax()const;
+       void showMin()const;
+
+        private:
+        std::priority_queue<double> maxHeap;
+        std::priority_queue<double,std::vector<double>,std::greater<double>> minHeap;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
+#include<vector>
+#include<queue>
 #include<string>
 #include<sstream>
 #include"PriceStream.hpp"
@@ -21,10 +23,36 @@ int PriceStream::GetPriceCount()const{
     return price.size();
 }
 
+
+void MaxMinTracker::storePrice(double price) {
+    maxHeap.push(price);
+    minHeap.push(price);
+}
+
+void MaxMinTracker::showMax() const {
+    if (!maxHeap.empty()) {
+        cout<<"Current Max Price :"<<maxHeap.top()<<endl;
+    } else {
+        cout << "No Prices Available" <<endl;
+    }
+}
+
+void MaxMinTracker::showMin() const {
+    if (!minHeap.empty()) {
+        cout<<"Current Min Price :"<<minHeap.top()<<endl;
+    } else {
+        cout << "No Prices Available" << endl;
+    }
+}
+
 int main (){
   
    int n;
-   cout <<" DSA Project - Real-Time-Stock-Price-Intelligence-System"<<endl;
+
+   PriceStream ps;
+   MaxMinTracker mm;
+
+   cout <<"DSA Project - Real-Time-Stock-Price-Intelligence-System"<<endl;
    cout<<" -----------------------------------------------------"<<endl;
    cout<<" | 1. Price Stream Management                        |"<<endl;
    cout<<" | 2. Max/Min Price Tracking                         |"<<endl;            
@@ -39,7 +67,7 @@ int main (){
    cout<<" -----------------------------------------------------"<<endl;
 
    while(true){
-    cout<<"Enter your Choice (0-9) : ";
+    cout<<"Enter your Choice (1-10) : ";
     string line;
     getline(cin, line);
     stringstream ss(line);
@@ -50,7 +78,6 @@ int main (){
     switch(n){
         case 1:{
             cout<<" Price Stream Management Selected "<<endl;
-            PriceStream ps;
             int subchoice;
             while(true){
                 cout << "1. Add Prices (multiple separated by spaces)" << endl;
@@ -65,16 +92,17 @@ int main (){
                     continue;
                 }
                 if(subchoice == 1){
-                    cout << "Enter prices separated by spaces: ";
+                    cout << "Enter prices (separated by spaces) : ";
                     getline(cin, line);
                     stringstream ss2(line);
                     double p;
                     int count = 0;
                     while(ss2 >> p){
                         ps.AddPrice(p);
+                        mm.storePrice(p);
                         count++;
                     }
-                    cout << count << " prices added successfully." << endl;
+                    cout << count << "Prices added successfully." << endl;
                 } else if(subchoice == 2){
                     cout << "Current price count: " << ps.GetPriceCount() << endl;
                 } else if(subchoice == 3){
@@ -87,6 +115,8 @@ int main (){
         }
         case 2:
             cout<<" Max/Min Price Tracking Selected "<<endl;
+            mm.showMax();
+            mm.showMin();
             break;
         case 3:
             cout<<" Median Price Tracking Selected "<<endl;
