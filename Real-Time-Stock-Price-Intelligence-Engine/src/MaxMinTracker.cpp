d98fa1b63425a23a5092ce6c82a625843e56cdbb
jay-karale <jaykarale06@gmail.com>
2026-01-25 22:02:22 +0530

    Optimize MaxMinTracker using priority queues

--- Real-Time-Stock-Price-Intelligence-Engine/src/MaxMinTracker.cpp
+++ Real-Time-Stock-Price-Intelligence-Engine/src/MaxMinTracker.cpp
@@ -1,5 +1,6 @@
 #include "MaxMinTracker.hpp"
 #include <iostream>
+#include <queue>
 using namespace std;
 
 void MaxMinTracker::showMaxMin(const vector<double>& prices) const {
@@ -8,11 +9,8 @@ void MaxMinTracker::showMaxMin(const vector<double>& prices) const {
         return;
     }
 
-    double mx = prices[0], mn = prices[0];
-    for (double p : prices) {
-        if (p > mx) mx = p;
-        if (p < mn) mn = p;
-    }
+    priority_queue<double> maxHeap(prices.begin(), prices.end());
+    priority_queue<double, vector<double>, greater<double>> minHeap(prices.begin(), prices.end());
 
-    cout << "Max Price: " << mx << "\nMin Price: " << mn << endl;
+    cout << "Max Price: " << maxHeap.top() << "\nMin Price: " << minHeap.top() << endl;
 }
